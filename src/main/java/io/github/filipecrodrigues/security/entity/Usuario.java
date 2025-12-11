eedf862cf538b4874da4d9c4ab5f8a654fdb21e4
Filipe <filipe.candido.rodrigues@gmail.com>
2025-12-11 09:42:51 -0300

    refactor: ajustar configurações e entidades de usuário
    - Atualiza o pom.xml com novas configurações/propriedades
    - Ajusta a entidade Usuario.java
    - Atualiza application.properties
    - Altera e renomeia tabelas através das migrações:
      - V2__rename_users_to_usuario.sql
      - V3__rename_usuario_to_usuarios.sql
    
    Objetivo: padronizar nomenclatura e manter consistência entre entidade, propriedades e migrações.

--- src/main/java/io/github/filipecrodrigues/security/entity/Usuario.java
+++ src/main/java/io/github/filipecrodrigues/security/entity/Usuario.java
@@ -1,4 +1,65 @@
 package io.github.filipecrodrigues.security.entity;
 
-public class Usuario {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="usuarios")
+public class Usuario implements UserDetails { //User Detaisl interfasse de contraos de segurança
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //gerada pelo banco
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {  //
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() { // qual é o usuario nome ou email etc
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
